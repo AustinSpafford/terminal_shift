a7569add3ddb8f9bb8fc969187e356f8a8bd4cfa
Austin Spafford <austin.spafford@gmail.com>
2016-04-15 22:35:10 -0700

    Adding a player platform and drop shadow

---
+++ Assets/Player/DropShadowOrienter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Text;
+
+public class DropShadowOrienter : MonoBehaviour
+{
+	public Vector3 FacingDirection = Vector3.down;
+
+	public void LateUpdate()
+	{
+		transform.rotation = 
+			Quaternion.LookRotation(
+				FacingDirection, 
+				(transform.parent.rotation * Vector3.forward));
+	}
+}
+
