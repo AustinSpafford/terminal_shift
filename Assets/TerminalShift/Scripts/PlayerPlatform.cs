096a7c24aedcb7635521ee9be87db0ba7426da6d
Austin Spafford <austin.spafford@gmail.com>
2016-04-16 00:06:59 -0700

    Basic scrolling elevator shaft
    There's a crude segmented elevator shaft that the player's platform now
    plummets through.

---
+++ Assets/TerminalShift/Scripts/PlayerPlatform.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Text;
+
+public class PlayerPlatform : MonoBehaviour
+{
+	public float CurrentSpeed = 0.0f;
+	public float TerminalSpeed = 50.0f;
+
+	public void Awake()
+	{
+		scrollingElevatorShaft = FindObjectOfType<ScrollingElevatorShaft>();
+	}
+
+	public void Update()
+	{
+		CurrentSpeed = 
+			Mathf.Min(
+				(CurrentSpeed + (Physics.gravity.magnitude * Time.deltaTime)),
+				TerminalSpeed);
+
+		scrollingElevatorShaft.AdvanceShaft(CurrentSpeed * Time.deltaTime);
+	}
+
+	private ScrollingElevatorShaft scrollingElevatorShaft = null;
+}
+
